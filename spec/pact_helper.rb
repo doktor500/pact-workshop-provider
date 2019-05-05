bc2e768851fcce759e5a64620823905447c1fba0
David Molinero <davidmolinero.com@gmail.com>
2019-05-05 11:52:32 +0100

    Update readme and include files from step2

--- spec/pact_helper.rb
+++ spec/pact_helper.rb
@@ -5,3 +5,12 @@ Pact.service_provider "PaymentService" do
     pact_uri '../pact-workshop-consumer/spec/pacts/paymentserviceclient-paymentservice.json'
   end
 end
+
+Pact.provider_states_for "PaymentServiceClient" do
+  provider_state "a black listed payment method" do
+    set_up do
+      invalid_payment_method = "9999999999999999"
+      PaymentMethodRepository.instance.black_list(invalid_payment_method)
+    end
+  end
+end
